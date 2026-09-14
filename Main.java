c9bafa17e47a0b9cabe277c08b36798b49296834
IvoKuban <146112708+IvoKuban@users.noreply.github.com>
2026-09-14 10:35:34 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,42 +4,56 @@ import java.util.Scanner;
 
 public class Main {
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean x = true;
+
+        while (x) {
+
             System.out.println("Prve cislo: ");
             int prve = scanner.nextInt();
             System.out.println("Druhe cislo: ");
             int druhe = scanner.nextInt();
             System.out.println("Operacia (1 pre +) (2 pre -) (3 pre /) (4 pre *): ");
             int operacia = scanner.nextInt();
-            int a=1;
-            int b=2;
-            int c=3;
-            int d=4;
-            if (operacia==a)
-            {
-                System.out.printf("Vysledok je: ");
+
+            int a = 1;
+            int b  = 2;
+            int c = 3;
+            int d = 4;
+
+            if (operacia == a) {
+                System.out.println("Vysledok je: ");
                 System.out.println(prve + druhe);
             }
-            else if (operacia== b)
-            {
-                System.out.printf("Vysledok je: ");
+            else if (operacia == b) {
+                System.out.println("Vysledok je: ");
                 System.out.println(prve - druhe);
             }
-            else if (operacia==c)
-            {
-                System.out.printf("Vysledok je: ");
+            else if (operacia == c) {
+                System.out.println("Vysledok je: ");
                 System.out.println(prve / druhe);
             }
-            else if (operacia==d)
-            {
-                System.out.printf("Vysledok je: ");
+            else if (operacia == d) {
+                System.out.println("Vysledok je: ");
                 System.out.println(prve * druhe);
             }
-            else{
-                System.out.printf("Neexistuje tato operacia");
+            else {
+                System.out.println("Neexistuje tato operacia");
+            }
+
+            scanner.nextLine();
+
+            System.out.println("Zastavit? ano/nie");
+            String odpoved = scanner.nextLine();
+
+            if (odpoved.equals("ano")) {
+                x = false;
             }
         }
 
-
+        System.out.println("Kalkulacka bola zastavena.");
+    }
 }
